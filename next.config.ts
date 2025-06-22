f983e98da560fa869fad9ec99ea142a961775b8d
Abdul Rafay <130937186+M-Abdul-RAFAY@users.noreply.github.com>
2025-06-22 17:26:25 +0500

    testing with ngork

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://192.168.157.1:3000",
+    "https://5d8c-2400-adc5-12d-9100-2c8e-efba-ae84-72f1.ngrok-free.app",
+  ],
 };
 
 export default nextConfig;
